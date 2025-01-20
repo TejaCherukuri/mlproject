23c525ad548552e0d40b27635b1daf58ca1b6513
TejaCherukuri <tejkrish98@gmail.com>
2025-01-20 15:07:18 -0500

    Add Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 from src.logger import logging
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, object):
     try:
@@ -11,16 +12,23 @@ def save_object(file_path, object):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, X_test, y_train, y_test, models):
+def evaluate_models(X_train, X_test, y_train, y_test, models, params):
     try:
         r2_list = {}
         for i in range(len(list(models))):
 
             model_name = list(models.keys())[i]
             model = list(models.values())[i]
-            logging.info(f"=====Training on {model_name}======")
+            param = params[list(params.keys())[i]]
 
-            model.fit(X_train, y_train)
+            logging.info(f"Hyper Parameter Tuning using GridSearchCV on {model_name}")
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            logging.info(f"=====Training on {model_name}======")
+            model.fit(X_train,y_train)
+
             logging.info(f"=====Predicting on {model_name}======")
             y_pred = model.predict(X_test)
 
